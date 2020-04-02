8d3c7224245d09d3b0c43973f889a4b2636ea854
02KellyV <900@holbertonschool.com>
2020-04-02 01:47:37 +0000

    fix

--- models/review.py
+++ models/review.py
@@ -14,5 +14,5 @@ class Review(BaseModel, Base):
     """
     __tablename__ = "reviews"
     user = Column(String(1024), nullable=False)
-    place_id = Column(String(60), nullable=False, ForeignKey(places.id))
-    user_id = Column(String(60), nullable=False, ForeignKey(users.id))
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
