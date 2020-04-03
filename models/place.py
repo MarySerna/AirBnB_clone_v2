90233114b09efb145372dd31d4844a93c59f09b6
MarySerna <mary.serna.8903@gmail.com>
2020-04-03 03:51:43 +0000

    fix place

--- models/place.py
+++ models/place.py
@@ -5,11 +5,13 @@ from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 import os
 
+
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60),
                              ForeignKey('places.id')),
-                      Column('amenity_id', String(60),                        
+                      Column('amenity_id', String(60),
                              ForeignKey('amenities.id')))
+
 
 class Place(BaseModel, Base):
     """
