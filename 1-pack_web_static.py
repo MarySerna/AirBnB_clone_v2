5415322c0a0d010541266843841b49f3fea3e9c1
MarySerna <mary.serna.8903@gmail.com>
2020-04-11 00:28:46 +0000

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of
+the web_static
+"""
+
+import os.path
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static
+    """
+
+    dtime = datetime.utcnow()
+    archipath = "versions/web_static_{}{}{}{}{}{}.tgz".format(dtime.year,
+                                                          dtime.month,
+                                                          dtime.day,
+                                                          dtime.hour,
+                                                          dtime.minute,
+                                                          dtime.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(archipath)).failed is True:
+        return None
+    return archipath
