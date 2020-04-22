73232fae7cde28a5f00789939c718dd4ee6d3c5a
MarySerna <mary.serna.8903@gmail.com>
2020-04-22 22:47:43 +0000

    state

--- models/state.py
+++ models/state.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
-""" holds class State"""
-import models
+"""This is the state class"""
 from models.base_model import BaseModel, Base
-from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+import os
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
+    """
+    This is the class for State
+    Attributes:
+    name: input name
+    """
+    __tablename__ = "states"
+    if os.environ.get('HBNB_TYPE_STORAGE') == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City",
+                              cascade="all, delete",
+                              backref="my_state")
     else:
         name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        name = ""
 
-    if models.storage_t != "db":
         @property
         def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
+            cities_dict = models.storage.all(City)
+            cities_list = []
+            for city in cities_dict.values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    cities_list.append(city)
+            return cities_list
+    else:
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+
+
