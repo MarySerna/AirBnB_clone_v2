f916c40e77d6dd965b1517109dc45e810f9b6d6a
MarySerna <mary.serna.8903@gmail.com>
2020-04-23 03:30:03 +0000

    hbnbfilters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -6,8 +6,15 @@ starts a Flask web application
 from flask import Flask, render_template
 from models import *
 from models import storage
+
+
 app = Flask(__name__)
 
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def filters():
@@ -16,12 +23,6 @@ def filters():
     amenities = storage.all("Amenity").values()
     return render_template('10-hbnb_filters.html', states=states,
                            amenities=amenities)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
